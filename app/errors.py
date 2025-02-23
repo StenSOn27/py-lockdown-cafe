0415ea0efc82618cb609002f5725222de98c209e
Taras Shevchyk <tarashevchik27@gmail.com>
2025-02-24 00:56:52 +0200

    solution

---
+++ app/errors.py
@@ -0,0 +1,21 @@
+﻿class VaccineError(Exception):
+    """Base class for other exceptions"""
+    pass
+
+
+class NotVaccinatedError(VaccineError):
+    def __init__(self, message: str = "Person is not vaccinated") -> None:
+        self.message = message
+        super().__init__(self.message)
+
+
+class OutdatedVaccineError(VaccineError):
+    def __init__(self, message: str = "Vaccine is outdated") -> None:
+        self.message = message
+        super().__init__(self.message)
+
+
+class NotWearingMaskError(Exception):
+    def __init__(self, message: str = "Person is not wearing a mask") -> None:
+        self.message = message
+        super().__init__(self.message)
